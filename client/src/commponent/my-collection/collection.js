b5d9cadff5c5d3af4429c3ab3c46d2e75956aaee
ilhamfathoni2 <altakhzin.ham@gmail.com>
2021-11-25 20:59:24 +0700

    Modif ui flex in collection

--- client/src/commponent/my-collection/collection.js
+++ client/src/commponent/my-collection/collection.js
@@ -10,37 +10,37 @@ function DataCollection() {
       <Container>
         <h1 className="title-litera mt-5 mb-4">My Collection</h1>
         <div className="d-flex flex-wrap justify-content-between">
-          <div className="space">
+          <div className="spaces">
             <Image src={tumbnal} />
             <h5 className="title-book mt-3">Management Sistem</h5>
             <p className="author">Rizky E </p>
             <p className="since">2020</p>
           </div>
-          <div className="space">
+          <div className="spaces">
             <Image src={tumbnal} />
             <h5 className="title-book mt-3">Management Sistem</h5>
             <p className="author">Rizky E </p>
             <p className="since">2020</p>
           </div>
-          <div className="space">
+          <div className="spaces">
             <Image src={tumbnal} />
             <h5 className="title-book mt-3">Sistem Informasi Standar BAN-PT</h5>
             <p className="author">Rizky E </p>
             <p className="since">2020</p>
           </div>
-          <div className="space">
+          <div className="spaces">
             <Image src={tumbnal} />
             <h5 className="title-book mt-3">Management Sistem</h5>
             <p className="author">Rizky E </p>
             <p className="since">2020</p>
           </div>
-          <div className="space">
+          <div className="spaces">
             <Image src={tumbnal} />
             <h5 className="title-book mt-3">Management Sistem</h5>
             <p className="author">Rizky E </p>
             <p className="since">2020</p>
           </div>
-          <div className="space">
+          <div className="spaces">
             <Image src={tumbnal} />
             <h5 className="title-book mt-3">Management Sistem</h5>
             <p className="author">Rizky E </p>
